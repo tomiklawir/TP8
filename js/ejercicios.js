ea18e63c313c6a8b427c9c6ca4e5373cbbf3db5a
TOMAS DAVID KLAWIR <48385206@ort>
2024-10-07 09:41:12 -0300

    Calcular edad hecho

---
+++ js/ejercicios.js
@@ -0,0 +1,42 @@
+// TIRANDO FRUTA
+function tirandoFruta(){
+    const frutas = {
+        "melon",
+        "guayaba",
+        "mango",
+        "anana",
+        "frambuesa",
+        "maracuya",
+        "sandia",
+        "papaya",
+        "nispero"
+    };
+
+
+    const frutaUsuario = prompt("Ingrese una fruta").toLowerCase();
+
+    if(frutas.includes(frutaUsuario))
+        console.log('Tenemos ${frutausuario}');
+
+    else
+        console.log('Tenemos ${frutausuario}');
+}
+
+//EJERCICIO CALCULADORA DE EDAD
+function calcularEdad(fechaNacimiento) {
+    const hoy = new Date();
+    const nacimiento = new Date(fechaNacimiento);
+    let edad = hoy.getFullYear() - nacimiento.getFullYear();
+    const mesActual = hoy.getMonth();
+    const mesNacimiento = nacimiento.getMonth();
+
+    if (mesActual < mesNacimiento || (mesActual === mesNacimiento && hoy.getDate() < nacimiento.getDate())){
+        edad--;
+    }
+    return edad;
+}
+
+const nombre = prompt("Ingrese su nombre");
+const fechaNacimiento = prompt("Ingrese su fecha de nacimiento (Formato: YYYY-MM-DD)");
+const edad = calcularEdad(fechaNacimiento);
+console.log(`Hola ${nombre}, tienes ${edad} años!`);
