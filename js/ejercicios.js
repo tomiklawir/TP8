d3b1532b25403ae2633a0b5d0a913b9162731533
TOMAS DAVID KLAWIR <48385206@ort>
2024-10-07 09:45:58 -0300

    Comparando datos y tipos hecho

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -22,7 +22,7 @@ function tirandoFruta(){
         console.log('Tenemos ${frutausuario}');
 }
 
-//EJERCICIO CALCULADORA DE EDAD
+// EJERCICIO CALCULADORA DE EDAD
 function calcularEdad(fechaNacimiento) {
     const hoy = new Date();
     const nacimiento = new Date(fechaNacimiento);
@@ -40,3 +40,10 @@ const nombre = prompt("Ingrese su nombre");
 const fechaNacimiento = prompt("Ingrese su fecha de nacimiento (Formato: YYYY-MM-DD)");
 const edad = calcularEdad(fechaNacimiento);
 console.log(`Hola ${nombre}, tienes ${edad} años!`);
+
+// COMPARANDO DATOS Y TIPOS
+let numero
+let numero2
+numero = (10 =='10') 
+numero2 = (10 ==='10') 
+console.log("el numero 10.6 es de tipo " + typeof(10.6))
